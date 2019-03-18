776dd9f1cc1bad0dc46ce3d5481b75d24fe54c09
Pete A <peter.ayresmusic@gmail.com>
2019-03-18 22:04:50 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,6 +2,6 @@ class Hash
   def keys_of(arguments)
     array = *arguments
     panama = * "red-footed tortoise"
-    madagascar = * "aye-aye","tomato frog"
+    madagascar = if * puts "aye-aye","tomato frog"
   end
 end
