5700c011c83b7c7233ff45b3bc9e9a6876aa8727
Pete A <peter.ayresmusic@gmail.com>
2019-03-18 22:27:14 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,10 @@
 class Hash
   def keys_of(*arguments)
-    
+    array = []
+    self.each do |key,value|
+    if arguments.include?(value)
+        array << key
+    end
   end
+  return array
 end
